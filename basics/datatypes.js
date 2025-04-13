ae62fe1d62c5214ea11d9d78a5beb5d055af1455
Sricharan Tuduru <142130899+SricharanTuduru@users.noreply.github.com>
2025-04-13 13:54:55 +0000

    datatypes and variables

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version 
+
+
+let name = "sricharan" //string
+let age = 21 //number 
+let isloggedin = false //boolean 
+
+//number => 2 to the power of 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol =>unique
+
+//object 
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// accountId = 2 // not allowed
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
